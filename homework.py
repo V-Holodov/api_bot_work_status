949d5589f4dfe3bd09644c847bc1003100058ac0
Vitalij Holodov <w.a.holodov@yandex.ru>
2020-12-06 16:08:45 +0500

    Added a request for the latest homework

--- homework.py
+++ homework.py
@@ -5,6 +5,7 @@ import json
 
 import requests
 import telegram
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from dotenv import load_dotenv
 
 logging.basicConfig(
@@ -21,6 +22,56 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 SLEEP_GET = 1200
 SLEEP_ERROR = 5
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+updater.start_polling()
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я твой ассистент проверки домашки")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+def recent_homwork():
+    params = {'from_date': 0}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    try:
+        homework_statuses = requests.get(
+            URL,
+            params=params,
+            headers=headers)
+        response = homework_statuses.json()
+        name = response.get('homeworks')[0]['homework_name']
+        comment = response.get('homeworks')[0]['reviewer_comment']
+        date = response.get('homeworks')[0]['date_updated']
+        return f'Домашняя работа: {name},\n{comment},\nДата: {date}'
+    except RequestException as e:
+        logger.error(f'ошибка соединения - {e}')
+    except json.JSONDecodeError as e:
+        logger.error(f'ошибка файла JSON - {e}')
+
+
+def check_recent_homwork(update, context):
+    text = recent_homwork()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    logger.info('Запрос комментария последней работы')
+
+
+check_recent_homwork_handler = CommandHandler('check', check_recent_homwork)
+dispatcher.add_handler(check_recent_homwork_handler)
 
 
 def parse_homework_status(homework):
